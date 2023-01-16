50edcc1c71eb70d94d49a9fe2046a459867fc700
Yehor Krupiei <yehorkrupiei@gmail.com>
2023-01-16 23:52:47 +0200

    demo v1.0

--- One Piece Card Game/ViewController.swift
+++ One Piece Card Game/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
+    }
+    
+    func flipButton(charchter: UIImage, button: UIButton) {
+        if button.currentImage == charchter {
+            button.setImage(UIImage(named: "flag"), for: .normal)
+        } else {
+            button.setImage(charchter, for: .normal)
+        }
+    }
+        
+    let charachterCollection =
+    [UIImage(named: "luffy"),
+     UIImage(named: "usopp"),
+     UIImage(named: "brook"),
+     UIImage(named: "whitebeard"),
+     UIImage(named: "zoro"),
+     UIImage(named: "usopp"),
+     UIImage(named: "luffy"),
+     UIImage(named: "brook"),
+     UIImage(named: "whitebeard"),
+     UIImage(named: "zoro")]
+        
+        
+        
+        @IBOutlet weak var touchLabel: UILabel!
+        @IBOutlet var buttonCollection: [UIButton]!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+        }
+        
+        @IBAction func buttonAction(_ sender: UIButton) {
+            touches += 1
+            let buttonIndex = buttonCollection.firstIndex(of: sender)!
+            flipButton(charchter: charachterCollection[buttonIndex]!, button: sender)
+        }
     }
 
-
-}
 
